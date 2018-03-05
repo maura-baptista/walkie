d3434f2a0414fc949233d85dafa17a9e2ad2ee88
Francisco Barreto <franciscoantunesbarreto1997@gmail.com>
2018-03-05 14:44:49 +0000

    Created the walks routes and started implementing the algolia search

---
+++ app/models/walk.rb
@@ -0,0 +1,20 @@
+class Walk < ApplicationRecord
+  belongs_to :user
+  has_many :points_of_interest
+  has_many :reviews
+  has_many :likes
+  has_many :user_walks
+  has_many :users, through: :user_walks
+
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :location, presence: true
+  validates :duration, presence: true
+  validates :description, presence: true
+  validates :photo, presence: true
+
+  include AlgoliaSearch
+  algoliasearch per_environment: true do
+    attribute :category
+  end
+end
