54b619339e6da83b80d888fd0285e94b3b91c032
Francisco Barreto <franciscoantunesbarreto1997@gmail.com>
2018-03-06 14:26:19 +0000

    iojfiodjio

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :walks, only: [ :index, :show, :search, :new, :create, :edit, :update ] do
+  resources :walks, only: [ :index, :show, :new, :create, :edit, :update ] do
     resources :user_walks, only: [ :index, :create ]
   end
 end
