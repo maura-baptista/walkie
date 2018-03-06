c24488dafc7156f6d2ef309f83a5273df049126a
maurabaptista <maura.mendes.baptista@gmail.com>
2018-03-06 17:27:06 +0000

    nothing done

--- db/schema.rb
+++ db/schema.rb
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 20180305181433) do
 
   create_table "walks", force: :cascade do |t|
     t.string "name"
+    t.string "category"
     t.string "location"
     t.string "duration"
     t.string "description"
