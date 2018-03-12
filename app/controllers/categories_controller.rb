8245b236c97672ccd3966f0ef0c9366ec5c48f80
Irina Popa <irina.oana.popa@gmail.com>
2018-03-12 10:54:18 +0000

    yes, time asc, desc works

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,10 +2,12 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
     @walks = Walk.all
+    @walks = Walk.order(duration: params[:sort].to_sym)
+
   end
 
   def show
     @category = Category.find(params[:id])
-    @walks = Walk.where(category: params[:id])
+    @walks = Walk.where(category: params[:id]).order(duration: params[:sort].to_sym)
   end
 end
