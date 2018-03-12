d048cdda45efa9392922721377a601245a33f4e7
Francisco Barreto <franciscoantunesbarreto1997@gmail.com>
2018-03-12 18:16:19 +0000

    lots of stuff changed in the master

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,11 @@
 import "bootstrap";
 
+$(document).ready(function(){
+
+// Fucking crazy animation created by the one and only francisco barreto , the king
+  setTimeout(
+    function() {
+      $('.alert').slideUp();
+  }, 2000);
+})
 
