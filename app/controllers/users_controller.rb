b20d7bba9410b58079ccf34ed7c3847b688336b8
jose monteiro <joemonteiro@verizon.net>
2018-03-13 15:46:18 +0000

    changed input field to pink

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def profile
-
+    @walks = current_user.likes.map { |like| like.walk }
+    @walk = Walk.where(user: current_user)
+    @walks_previous = current_user.walks
   end
 end
